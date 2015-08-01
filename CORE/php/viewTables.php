99afa858df5610853b1a6c8f91bb8a4ba3f0180c
Tyler D'Spain <vandyke666@gmail.com>
2015-07-31 23:14:25 -0500

    only a view for recently loaded data

---
+++ CORE/php/viewTables.php
@@ -0,0 +1,80 @@
+<?php
+$servername = "localhost";
+$username = "test_user";
+$password = "password";
+$dbname = "db_rental";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname); 
+
+$sql = "SELECT * FROM car";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table class='tables' border='1'><tr style='text-align: left'><th>VehicleID</th><th>Model</th><th>Year</th><th>Availability</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["VehicleID"]."</td><td>".$row["Model"]."</td><td>".$row["Year"]."</td><td>".$row["Availability"]."</td></tr>";
+    }
+    echo "</table><br>";
+} else {
+    echo "0 results<br>";
+}
+
+$sql = "SELECT * FROM car_type";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table class='tables' border='1'><tr style='text-align: left'><th>VID</th><th>TName</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["VID"]."</td><td>".$row["TName"]."</td></tr>";
+    }
+    echo "</table><br>";
+} else {
+    echo "0 results<br>";
+}
+
+$sql = "SELECT * FROM customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table class='tables' border='1'><tr style='text-align: left'><th>IdNo</th><th>Name</th><th>Phone</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["IdNo"]."</td><td>".$row["Name"]."</td><td>".$row["Phone"]."</td></tr>";
+    }
+    echo "</table><br>";
+} else {
+    echo "0 results<br>";
+}
+
+$sql = "SELECT * FROM rental";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table class='tables' border='1'><tr style='text-align: left'><th>Status</th><th>VehicleID</th><th>CustID</th><th>Daily</th><th>Weekly</th><th>StartDate</th><th>NoOfDays</th><th>ReturnDate</th><th>AmountDue</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["Status"]."</td><td>".$row["VehicleID"]."</td><td>".$row["CustID"]."</td><td>".$row["Daily"]."</td><td>".$row["Weekly"]."</td><td>".$row["StartDate"]."</td><td>".$row["NoOfDays"]."</td><td>".$row["ReturnDate"]."</td><td>".$row["AmountDue"]."</td></tr>";
+    }
+    echo "</table><br>";
+} else {
+    echo "0 results<br>";
+}
+
+$sql = "SELECT * FROM type";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table class='tables' border='1'><tr style='text-align: left'><th>TypeName</th><th>DailyRate</th><th>WeeklyRate</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["TypeName"]."</td><td>".$row["DailyRate"]."</td><td>".$row["WeeklyRate"]."</td></tr>";
+    }
+    echo "</table><br>";
+} else {
+    echo "0 results<br>";
+}
+$conn->close();
+?>
