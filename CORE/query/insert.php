a1d9eadb4508172834b9b59456f7f97465f99f73
Tyler D'Spain <vandyke666@gmail.com>
2015-08-06 00:43:40 -0500

    somewhat working insert.php customer works, car....works. rental doesn't

--- CORE/query/insert.php
+++ CORE/query/insert.php
@@ -7,19 +7,50 @@ $dbname = "db_rental";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
 
-$table = null;
-$param1 = null;
-$param2 = null;
-$param3 = null;
+$table = $_POST['formTableName'];
 
+if(isset($_POST['formTableName'])) {
+	switch ($table) {
+		case "Customer":
+			$param1 = $_POST['cust_name'];
+			$param2 = $_POST['phone_num'];
+			$sql = "INSERT INTO " . $table . " (Name, Phone) VALUES ( '" . $param1 . "', '" . $param2 . "');";
 
-$sql = "INSERT INTO " . $table . " VALUES ( " . $param1 . ", " . $param2 . ", " . $param3 . ");";
-$result = $conn->query($sql);
+			if ($conn->query($sql) === TRUE) {
+				echo $param1 . ", " . $param2 . ", entered into table '" . $table . "' successfully<br>";
+			} else {
+				echo "Error creating record: " . $conn->error . "<br>";
+			}
+			break;
+		case "Car":
+			$param1 = $_POST['car_model'];
+			$param2 = $_POST['year_model'];
+			$sql = "INSERT INTO " . $table . " (Model, Year) VALUES ( '" . $param1 . "', '" . $param2 . "');";
 
-if ($conn->query($sql) === TRUE) {
-	echo $param1 . ", " . $param2 . ", " . $param3 . ", entered into table '" . $table . "' successfully<br>";
-} else {
-    echo "Error creating record: " . $conn->error . "<br>";
+			if ($conn->query($sql) === TRUE) {
+				echo $param1 . ", " . $param2 . ", entered into table '" . $table . "' successfully<br>";
+			} else {
+				echo "Error creating record: " . $conn->error . "<br>";
+			}
+			break;
+		case "Rental":
+			$param1 = $_POST['vehicle'];
+			$param2 = $_POST['id_number'];
+			$param3 = $_POST['period'];
+			$param4 = $_POST['days_weeks'];
+			if ($param3 == 'Daily') {
+				$sql = "INSERT INTO " . $table . " (VehicleID, CustID, Period, NoOfDays) VALUES ( " . $param1 . ", " . $param2 . ", '" . $param3 . "', '" . $param4 . ");";
+			}
+			if ($param3 == 'Weekly') {
+				$sql = "INSERT INTO " . $table . " (VehicleID, CustID, Period, NoOfWeeks) VALUES ( " . $param1 . ", " . $param2 . ", '" . $param3 . "', '" . $param4 . ");";
+			}
+			if ($conn->query($sql) === TRUE) {
+				echo $param1 . ", " . $param2 . ", " . $param3 . ", " . $param4 . ", " . $param5 . ", " . $param6 . ", entered into table '" . $table . "' successfully<br>";
+			} else {
+				echo "Error creating record: " . $conn->error . "<br>";
+			}
+			break;
+	}
 }
 
 $conn->close();
