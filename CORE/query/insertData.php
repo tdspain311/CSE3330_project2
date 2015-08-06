aaab30cebd1ad13a19256f86e281c5ed8b7fc577
isha soni <isha.soni.18@gmail.com>
2015-08-05 19:31:20 -0500

    rough work - created a new_query.html that I could mess with and form that works with html and php without ajax

--- CORE/query/insertData.php
+++ CORE/query/insertData.php
@@ -9,81 +9,165 @@ $dbname = "db_rental";
 $conn = new mysqli($servername, $username, $password, $dbname); 
 
 echo "INSERT INFO";
-    
-if(isset($_POST['formSubmit']) )
-{
-    $tableName = $_POST['formTableName'];
-    $CustName = $_POST['cust_name'];
-    $CustID;
-    $Phone;
-    $VehicleID;
-    $Model;
-    $carType;
-    $Year;
-    $Availability;
-    $Status;
-    $Daily;
-    $Weekly;
-    $StartDate;
-    $NoOfDays;
-    $NoOfWeeks;
-    $ReturnDate;
-    $AmountDue;
+ 
+$table = $_POST["formTableName"];
+$Name = $_POST["cust_name"];
+$Phone = $_POST["phone_num"];
+$VID = $_POST["car_model"];
+$Model = $_POST["car_model"];
+$CarType = $_POST["carType"];
+$Year = $_POST["year_model"];
+$RentalVID = $_POST["vid"];
+$RentalCID = $_POST["cid"];
+$StartDate = date("Ymd");
+$Daily = $_POST["daily_rental"];
+$Weekly = $_POST["weekly_rental"];
+$NoOfDays = $_POST["days"];
+$NoOfWeeks = $_POST["weeks"];
+
+echo $table,$Name,$Phone;
+
+if(isset($_POST['formSubmit'])){
+    echo $table, $Name, $Phone;
     
     $errorMessage = "";
-
-    echo $CustName;
     
-    if(empty($tableName)) 
+    if(empty($table)) 
     {
-    $errorMessage = "<li>You forgot to select a table!</li>";
+        $errorMessage = "<li>You forgot to select a table!</li>";
     }
 
     if($errorMessage != "") 
     {
-    echo("<p>There was an error with your form:</p>\n");
-    echo("<ul>" . $errorMessage . "</ul>\n");
+        echo("<p>There was an error with your form:</p>\n");
+        echo("<ul>" . $errorMessage . "</ul>\n");
     } 
     else
     {
 
-    switch($tableName)
-    {
-      case "Customer": 
-        echo "Table Customer selected"; 
-        $sql = "INSERT INTO customer VALUES (" . $CustID . ", " . $CustName . ", " . $Phone . ");";
-        break;
-      case "Car": 
-        echo "Table Car selected"; 
-        $sql = "INSERT INTO car VALUES (" . $VehicleID . ", " . $Model . ", " . $Year . ", " . $Availability . ");";
-        break;
-      case "Rental": 
-        echo "Table Rental selected"; 
-        $sql = "INSERT INTO rental VALUES (" . $Status . ", " . $VehicleID . ", " . $CustID . "," . $Daily . "," . $Weekly . ", " . $StartDate . "," . $NoOfDays . "," . $NoOfWeeks . ", " . $ReturnDate . ", " . $AmountDue . ");";
+        switch($table)
+        {
+          case "Customer": 
+            echo "Table Customer selected"; 
+            $sql = "INSERT INTO customer(Name,Phone) VALUES ('$Name','$Phone')";
 
-        break;
-      default: 
-        echo("Error!"); 
-        exit(); 
-        break;
-    }
+            if ($conn->query($sql) === TRUE) {
+                echo $Name . ", " . $Phone . "  added successfully<br>";
+            } else {
+                echo "Error adding data: " . $conn->error . "<br>";
+            }
+            break;
+          case "Car": 
+            echo "Table Car selected"; 
+            $sql = "INSERT INTO car(Model,Year,Availability) VALUES ('$Model','$Year','Yes')";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo $Model . ", " . $Year . ", " . $Availability . " added successfully<br>";
+            } else {
+                echo "Error adding data: " . $conn->error . "<br>";
+            }
+            break;
+          case "Rental": 
+            echo "Table Rental selected"; 
+            $sql = "INSERT INTO rental(Status,VehicleID,CustID,Daily,Weekly,StartDate,NoOfDays,NoOfWeeks) VALUES ('Scheduled','$RentalVID','$RentalCID','$Daily','$Weekly','$StartDate','$NoOfDays','$NoOfWeeks')";
+            if ($conn->query($sql) === TRUE) {
+                echo $Status . ", " . $RentalVID . ", " . $RentalCID . "," . $Daily . "," . $Weekly . "," . $StartDate . "," . $NoOfDays . "," . $NoOfWeeks . "  added successfully<br>";
+            } else {
+                echo "Error adding data: " . $conn->error . "<br>";
+            }
+
+            break;
+          default: 
+            echo("Error!"); 
+            exit(); 
+            break;
+        }
 
     exit();
     } 
-    }
 
-    //$sql = "SELECT Name, Phone, Club FROM players WHERE country='USA'";
-    //$result = $conn->query($sql);
-    //
-    //if ($result->num_rows > 0) {
-    //    echo "<table border='1'><tr style='text-align: left'><th>Name</th><th>Position</th><th>Club</th></tr>";
-    //    // output data of each row
-    //    while($row = $result->fetch_assoc()) {
-    //        echo "<tr><td>".$row["Name"]."</td><td>".$row["Position"]."</td><td>".$row["Club"]."</td></tr>";
-    //    }
-    //    echo "</table>";
-    //} else {
-    //    echo "0 results";
-    //}
+}
+
+
+
+//if(isset($_POST['formSubmit']) )
+//{
+//    if(isset($_POST['customerName'])){
+//        echo $_POST['customerName'];
+//    }
+//    else{
+//        echo "nothing received";
+//    }
+//    $tableName = $_POST["formTableName"];
+//    $CustName = $_POST["customerName"];
+//    $CustID = $_POST["cust"];
+////    $Phone;
+////    $VehicleID;
+////    $Model;
+////    $carType;
+////    $Year;
+////    $Availability;
+////    $Status;
+////    $Daily;
+////    $Weekly;
+////    $StartDate;
+////    $NoOfDays;
+////    $NoOfWeeks;
+////    $ReturnDate;
+////    $AmountDue;
+//    
+//    $errorMessage = "";
+//    
+//    if(empty($tableName)) 
+//    {
+//    $errorMessage = "<li>You forgot to select a table!</li>";
+//    }
+//
+//    if($errorMessage != "") 
+//    {
+//    echo("<p>There was an error with your form:</p>\n");
+//    echo("<ul>" . $errorMessage . "</ul>\n");
+//    } 
+//    else
+//    {
+//
+//    switch($tableName)
+//    {
+//      case "Customer": 
+//        echo "Table Customer selected"; 
+//        $sql = "INSERT INTO customer VALUES (" . $CustID . ", " . $CustName . ", " . $Phone . ");";
+//        break;
+//      case "Car": 
+//        echo "Table Car selected"; 
+//        $sql = "INSERT INTO car VALUES (" . $VehicleID . ", " . $Model . ", " . $Year . ", " . $Availability . ");";
+//        break;
+//      case "Rental": 
+//        echo "Table Rental selected"; 
+//        $sql = "INSERT INTO rental VALUES (" . $Status . ", " . $VehicleID . ", " . $CustID . "," . $Daily . "," . $Weekly . ", " . $StartDate . "," . $NoOfDays . "," . $NoOfWeeks . ", " . $ReturnDate . ", " . $AmountDue . ");";
+//
+//        break;
+//      default: 
+//        echo("Error!"); 
+//        exit(); 
+//        break;
+//    }
+//
+//    exit();
+//    } 
+//}
+//
+//    //$sql = "SELECT Name, Phone, Club FROM players WHERE country='USA'";
+//    //$result = $conn->query($sql);
+//    //
+//    //if ($result->num_rows > 0) {
+//    //    echo "<table border='1'><tr style='text-align: left'><th>Name</th><th>Position</th><th>Club</th></tr>";
+//    //    // output data of each row
+//    //    while($row = $result->fetch_assoc()) {
+//    //        echo "<tr><td>".$row["Name"]."</td><td>".$row["Position"]."</td><td>".$row["Club"]."</td></tr>";
+//    //    }
+//    //    echo "</table>";
+//    //} else {
+//    //    echo "0 results";
+//    //}
     $conn->close();
 ?>
